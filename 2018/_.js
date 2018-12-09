56c041e31b4e6042a9023b9668229ca67bb32217
Johan Aschan <aschan.johan@gmail.com>
2018-12-09 17:34:10 +0100

    Day 6

---
+++ 2018/_.js
@@ -0,0 +1,12 @@
+const part1 = () => {
+    return input.split(/\n/);
+};
+
+const part2 = () => {
+    return input.split(/\n/);
+};
+
+const input = ``;
+
+console.log(part1());
+console.log(part2());
